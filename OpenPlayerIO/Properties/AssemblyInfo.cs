3737118379b83c21863efba176c5e0378a15a516
Atilla Lonny <contact.gh@atil.la>
2018-05-20 12:10:54 -0400

    OPIO.PlayerIOCleint v.1.3.2.2
    [^] User-Friendly Message IndexOutOfRangeException
    
    Co-Authored-By: SirJosh3917 <sirjosh3917@users.noreply.github.com>

--- OpenPlayerIO/Properties/AssemblyInfo.cs
+++ OpenPlayerIO/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OpenPIO.PlayerIOClient")]
-[assembly: AssemblyCopyright("Copyright © OpenPIO 2017")]
+[assembly: AssemblyCopyright("Copyright © OpenPIO 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.2.1")]
-[assembly: AssemblyFileVersion("1.3.2.1")]
+[assembly: AssemblyVersion("1.3.2.2")]
+[assembly: AssemblyFileVersion("1.3.2.2")]
